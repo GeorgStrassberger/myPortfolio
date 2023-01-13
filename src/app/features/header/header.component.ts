43bea76f54f48255d0f33ec5c1b6ac3600d9e83d
Georg <georg.strassberger@googlemail.com>
2023-01-13 10:55:10 +0100

    Adding Menu animation

--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -7,25 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   isOpen: boolean = false;
+  index: number = 0;
+  MENU_IMAGES: string[] = [
+    '../../../assets/images/menu/menu1.png',
+    '../../../assets/images/menu/menu2.png',
+    '../../../assets/images/menu/menu3.png',
+    '../../../assets/images/menu/menu4.png',
+    '../../../assets/images/menu/menu5.png'
+  ];
+  // menu = document.getElementById('menu')as HTMLImageElement;
   // kopf = document.getElementById('kopf') as HTMLElement;
   // top = document.getElementById('top') as HTMLDivElement;
   // bot = document.getElementById('bot') as HTMLDivElement;
-
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
   toggleMenu(){
-    if(this.isOpen){
-      this.closeMenu();
-    }else{
+    if(!this.isOpen){
       this.openMenu();
+      this.isOpen = true;
+    }else{
+      this.closeMenu();
+      this.isOpen = false;
     }    
   }
 
+
   openMenu(){
-    console.log('openMenu');
+    this.openMenuAnimation(this.index);
+
     let kopf = document.getElementById('kopf') as HTMLDivElement;
     let top = document.getElementById('top') as HTMLDivElement;
     let bot = document.getElementById('bot') as HTMLDivElement;
@@ -34,34 +47,52 @@ export class HeaderComponent implements OnInit {
     top.classList.add('animateContent');
     bot.classList.add('animateFooter');
 
-    this.isOpen = true;
     // this.kopf.classList.add('collapsible');
     // this.top.classList.add('animateContent');
     // this.bot.classList.add('animateFooter');
   }
 
   closeMenu(){
-    console.log('closeMenu');
+    this.closeMenuAnimation(this.index);
+
     let kopf = document.getElementById('kopf') as HTMLDivElement;
     let top = document.getElementById('top') as HTMLDivElement;
     let bot = document.getElementById('bot') as HTMLDivElement;
 
     setTimeout(()=>{
       kopf.classList.remove('collapsible');
-
     },1000)
     top.classList.remove('animateContent');
     bot.classList.remove('animateFooter');
+  }
 
-    this.isOpen = false;
+
+  openMenuAnimation(index: number) {
+    const menu = document.getElementById('menu')as HTMLImageElement;
+    const TIMER = setInterval(() => {
+        this.index++
+        menu.src = this.MENU_IMAGES[index];
+        if (this.index >= 4) {
+            clearInterval(TIMER);
+        }
+    }, 100);
+  }
+
+
+  closeMenuAnimation(index:number) {
+    const menu = document.getElementById('menu')as HTMLImageElement;
+    const TIMER = setInterval(() => {
+      this.index--
+      menu.src = this.MENU_IMAGES[index];
+      if (this.index <= 0) {
+          clearInterval(TIMER);
+      }
+    }, 100);
   }
 
 
   goToSection(sectionID:string){
     document.location = '#' + sectionID;
-    let container = document.getElementById(sectionID);
-    console.log('container:',container);
-
     this.closeMenu();
   }
 }
