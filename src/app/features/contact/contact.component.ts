16fb869bbc7089297a325fee3b412f4cf58dc9a7
Georg <georg.strassberger@googlemail.com>
2023-01-24 12:26:19 +0100

    adding Validation

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -8,11 +8,43 @@ import { NavigationService } from 'src/app/shared/services/navigation.service';
 })
 export class ContactComponent implements OnInit {
 
+ 
   constructor(
     public navService: NavigationService
   ) { }
 
   ngOnInit(): void {
+ 
   }
+
+
+  checkAllValid(){
+    this.isFieldValid('name');
+    this.isEmailValid('email');
+    this.isFieldValid('message');
+  }
+
+  isEmailValid(id:string):boolean{
+    let emailPattern: RegExp = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    let field = document.getElementById(id) as HTMLInputElement;
+    if(field.value.trim().length > 1 && field.value.match(emailPattern)){
+      console.log(id,' is VALID');
+      return true;
+    }else{
+      console.log(id,' is NOT valid')
+      return false;
+    }    
+  }
+
+  isFieldValid(id: string):boolean {
+    let field = document.getElementById(id) as HTMLInputElement | HTMLTextAreaElement;
+    if (field.value.trim().length > 1) {
+      console.log(id,' is VALID');
+      return true;
+    }else{
+      console.log(id,' is NOT valid');
+      return false;
+    }
+  }  
 
 }
