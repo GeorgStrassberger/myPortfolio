6c1d609938811556c38830af307ecf75c93c2456
Georg <georg.strassberger@googlemail.com>
2023-01-24 09:44:28 +0100

    clear Staging-Area

--- src/app/features/portfolio/project/project.component.ts
+++ src/app/features/portfolio/project/project.component.ts
@@ -1,9 +1,10 @@
+import { keyframes } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.scss']
+  styleUrls: ['./project.component.scss','@media.component.scss', '@keyframes.component.scss']
 })
 export class ProjectComponent implements OnInit {
 
