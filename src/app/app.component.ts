6f76bad613ced214c28ca22d5937661364425416
Georg <georg.strassberger@googlemail.com>
2023-03-31 15:37:41 +0200

    add plugIn for ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+// import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +7,10 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(public translate: TranslateService) {
-    // this language will be used as a fallback when a translation isn't found in the current language
-    // translate.setDefaultLang('en');
-    // the lang to use, if the lang isn't available, it will use the current loader to get them
-    // translate.use('en');
-  }
+  // constructor(public translate: TranslateService) {
+  // this language will be used as a fallback when a translation isn't found in the current language
+  // translate.setDefaultLang('en');
+  // the lang to use, if the lang isn't available, it will use the current loader to get them
+  // translate.use('en');
+  // }
 }
